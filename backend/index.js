6e728440f2b370c10cfed7e0ba3144dc449a2255
juanpchica <juanpchica@hotmail.com>
2021-08-09 17:43:10 -0500

    Installing debug dependencies server

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
+
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+const data = require("./data/db.json");
+
 app.use(express.json());
+app.use(morgan("dev"));
 app.use(
   cors({
     credentials: true,
@@ -15,7 +20,13 @@ app.use(
 );
 app.use(express.static("public"));
 
-app.get("/", (_, res) => res.send("Hello world!!"));
+app.get("/agents/", (req, res) => {
+  console.log("work");
+  if (req.params.income) {
+  } else {
+    return res.status(200).json({ agents: data });
+  }
+});
 
 app.listen(PORT, async () => {
   console.log(`Server is running at port ${PORT}`);
