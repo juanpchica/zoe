c157dcfe3f3f3698e3730d30261ef79614da10d2
juanpchica <juanpchica@hotmail.com>
2021-08-09 19:55:02 -0500

    Header mobile finished

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+//Assets
+import brand from "../images/zoe-brand.png";
 
 const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <nav className='navbar'>
+      <div className='navbar-container'>
+        <div className='navbar-brand'>
+          <Link to='/home'>
+            <img src={brand} alt='Zoe Financial' />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
