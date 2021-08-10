b73798917ec26c6c7bff42048147a9328b78b0a6
juanpchica <juanpchica@hotmail.com>
2021-08-10 16:14:58 -0500

    Removing compose redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
 import dataReducer from "./reducers/dataReducer";
@@ -14,10 +14,7 @@ const reducers = combineReducers({
 const store = createStore(
   reducers,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  applyMiddleware(...middleware)
 );
 
 export default store;
